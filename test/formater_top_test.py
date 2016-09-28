b64ff81eec01e882fdc4f4accb9391df90ec5090
wenqi <oar.yin@sjtu.edu.cn>
2016-09-28 15:22:57 +0800

    Test Buffer Ok

--- test/formater_top_test.py
+++ test/formater_top_test.py
@@ -38,7 +38,7 @@ if __name__ =='__main__':
 	reload(pySpice.global_data)
 	NETLIST_ROOT = 'data/sample_netlist/'
 	#parser(NETLIST_ROOT + 'pmos.sp')
-	parser(NETLIST_ROOT + 'cmos_inverter.sp')
+	parser(NETLIST_ROOT + 'buffer.sp')
 	raw_output = solve()
 	format(raw_output, 'testout.ls')
 
