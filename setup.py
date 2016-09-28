b27041ae41949693f2d28fd30a0d7f39cc02685c
wenqi <oar.yin@sjtu.edu.cn>
2016-09-28 20:55:10 +0800

    Finish Setup Sript

--- setup.py
+++ setup.py
@@ -2,10 +2,13 @@ from setuptools import setup
 
 setup(
     author = 'Wenqi Yin',
+    author_email = 'oar.yin@sjtu.edu.cn',
+    url = 'url',
+    license = 'MIT',
     version = '0.1',
     description = 'spice simulator implemented in python',
     install_requires = ['nose', 'numpy','matplotlib','getopt'],
-    packages = ['pySpice'],
+    packages = ['pySpice','test'],
     scripts = ['bin/pyspice','bin/result_viewer'],
     name = 'pySpice'
     )
